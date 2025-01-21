6fe3537112c86bccf462814ee64030522360493a
GamerTag <justagamert4g@gmail.com>
2025-01-20 23:04:50 -0500

    Update imports.py

--- imports.py
+++ imports.py
@@ -3,6 +3,5 @@ from tkinter import ttk
 from tkinter.simpledialog import askstring
 from tkinter.filedialog import asksaveasfilename, askopenfilename
 from tkinter.colorchooser import askcolor
-from tkinter.font import Font
 from tkinter import messagebox , Canvas, PhotoImage , colorchooser , scrolledtext , colorchooser, filedialog , simpledialog , Menu 
 from tkinter.font import Font, families
