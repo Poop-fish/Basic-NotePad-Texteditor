29bff0e346daff24c5dea87837616fbdc16e289f
GamerTag <justagamert4g@gmail.com>
2025-01-21 19:24:33 -0500

    Update main.py
    Added custom Style import to the widget

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from imports import *
-from GT_NotePad import GTextEditor
-
+from GT_NotePad import *
+from GT_Style import *
+  
 def main():
+
     root = tk.Tk()
-    root.title("Text Editor")
-    root.geometry("800x600")
+    
+    root.title("Random NotePad")
+    
+    root.geometry("1200x800")
+    
     root.iconbitmap("Assets/FaceLogo.ico")  
-    text_editor = GTextEditor(x=0, y=0, wd=800, ht=600, default_font=("Arial", 12), bg="white", fg="black")
-    text_editor._build(root)
+    
+    Apply_GTstyle(root)  #! \\ Apply the custom style (this effects the interface i.e buttons ,menus , etc ...) \\
+    
+    text_editor = GTextEditor(wd=800, ht=600, default_font=("Arial", 15), bg="black", fg="lime")
+    text_editor._build(root) 
     
     root.mainloop()
 
 if __name__ == "__main__":
     main()
+
