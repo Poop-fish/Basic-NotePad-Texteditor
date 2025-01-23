b181fc90113d5a84ce23df307933bb86a7741037
GamerTag <justagamert4g@gmail.com>
2025-01-23 00:20:04 -0500

    Update and rename main.py to Example.py
    re organized and updated

--- Example.py
+++ Example.py
@@ -1,8 +1,8 @@
-from imports import *
+from GT_imports import *
 from GT_NotePad import *
-from GT_Style import *
-  
-def main():
+
+# \\ This is how you can build the NotePad into a tk window \\
+def Example():
 
     root = tk.Tk()
     
@@ -12,13 +12,15 @@ def main():
     
     root.iconbitmap("Assets/FaceLogo.ico")  
     
-    Apply_GTstyle(root)  #! \\ Apply the custom style (this effects the interface i.e buttons ,menus , etc ...) \\
+    Apply_GTstyle(root)  #! \\ Apply the custom style (this effects the interface i.e buttons ,menus , etc ...) 
     
+    #! \\ Create a GTextEditor Widget and build it into the tk window \\
     text_editor = GTextEditor(wd=800, ht=600, default_font=("Arial", 15), bg="black", fg="lime")
-    text_editor._build(root) 
+    text_editor._build(root ) 
     
     root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    Example()
+
 
