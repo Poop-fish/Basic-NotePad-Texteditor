4dcc1faa46061d253d453d4e11414cde707c3c2c
GamerTag <justagamert4g@gmail.com>
2025-01-25 00:01:22 -0500

    Update GT_Style.py
    tweaked styles

--- GT_Style.py
+++ GT_Style.py
@@ -1,25 +1,26 @@
-from imports import *
+from GT_imports import *
 
 def Apply_GTstyle(root):
     style = ttk.Style(root)
     style.theme_use("alt")
-
+    
     #* \\ Notebook Custom Style (Tabbed Interface) \\
-    style.configure("TNotebook", background="lightgray", tabmargins=[5, 5, 5, 0])
-    style.configure("TNotebook.Tab", background="Gray", foreground="Black", padding=[10, 5], font=("Arial", 12))
+    style.configure("TNotebook", background="#a1a1a1", tabmargins=[5, 5, 5, 0])
+    style.configure("TNotebook.Tab" , background="Gray", foreground="Black", padding=[10, 5], font=("Arial", 12))
     style.map("TNotebook.Tab", background=[("selected", "Gray")])
 
     #* \\ Entry Widget Style \\
-    style.configure("TEntry", fieldbackground="Lime", foreground="Black", font=("Arial", 14))
-
+    style.configure("TEntry", fieldbackground="#444444", foreground="#00db00", font=("Arial", 14))
+    
     #* \\ Button Styles (Text and Tab Color Buttons) \\
-    style.configure("TButton", background="Gray", foreground="Black", font=("Arial", 14))
+    style.configure("TButton", background="Gray", foreground="Black", font=("Arial", 14) ,relief="ridge", borderwidth=10)
+    style.map("TButton", background=[("active", "darkgray")])
     
     #* \\ Scrollbar Style \\
-    style.configure("TScrollbar", background="black", troughcolor="Lime", arrowcolor="Lime")
+    style.configure("TScrollbar", background="black", troughcolor="#444444", arrowcolor="#00db00" )
     
     #* \\ OptionMenu Styles (Font Menu: Size and Font Buttons) \\
-    style.configure("TMenubutton", background="gray", foreground="Black", font=("Arial", 14))
+    style.configure("TMenubutton", background="gray", foreground="Black", font=("Arial", 14) ,relief="ridge", borderwidth=10)
     style.map("TMenubutton", background=[("active", "darkgray")])
     
     #* \\ Dropdown Menu Customization for OptionMenu \\
@@ -30,9 +31,9 @@ def Apply_GTstyle(root):
             fg="Black",             #* \\ Foreground (text) color
             font=("Arial", 12),  
             activebackground="light gray",  #* \\ Hover background
-            activeforeground="Lime"       #* \\ Hover text color
+            activeforeground="#00db00"       #* \\ Hover text color
         )
         return menu
-
+    
     return customize_menu 
 
