002b7786110cca3068a837ff8540eef68c05841a
GamerTag <justagamert4g@gmail.com>
2025-01-25 00:00:32 -0500

    Update GT_imports.py
    more imports

--- GT_imports.py
+++ GT_imports.py
@@ -1,6 +1,7 @@
+# \\ TKINTER IMPORTS \\
 import tkinter as tk
 
-from tkinter import ttk
+from tkinter import ttk , filedialog , colorchooser
 
 from tkinter.simpledialog import askstring
 
@@ -12,3 +13,24 @@ from tkinter.font import Font, families
 
 from tkinter import messagebox , Canvas, PhotoImage , colorchooser , scrolledtext , colorchooser, filedialog , simpledialog , Menu 
 
+
+# \\ BASE IMPORTS \\ 
+import math , webbrowser , random 
+
+import speech_recognition as sr
+
+from datetime import datetime 
+
+from spellchecker import SpellChecker
+
+from PIL import Image, ImageDraw, ImageTk
+
+
+
+# \\ FILE IMPORTS \\ 
+# from GT_keybinds import *
+# from GT_Tab_Management import *
+# from GT_Editor import * 
+# from GT_File_Operations import * 
+# from GT_Menu_Toolbar_Management import * 
+# from GT_Style import * 
